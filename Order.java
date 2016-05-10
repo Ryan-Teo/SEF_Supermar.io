697d0191cd5dc8179edc6104c10a0becd3c9448e
rmit-s3444615-ryan-teo <s3444615@student.rmit.edu.au>
2016-05-10 14:55:28 +1000

    Update Order.java

--- Order.java
+++ Order.java
@@ -1,14 +1,23 @@
 package order;
 
+import exceptions.NotFoundException;
+import product.FuncProduct;
+import product.Product;
+
 public class Order {
-	private String opID, opName, date;
-	private double qtyOrdered;
+	String opID, opName, date;
+	double qtyOrdered;
+	Product prod;
+	FuncProduct fProd = new FuncProduct();
 	
-	public Order(String opID, String opName, double qtyOrdered, String date){
+	public Order(String opID, String opName, double qtyOrdered, String date) throws NotFoundException{
 		this.opID=opID;
 		this.opName=opName;
 		this.qtyOrdered=qtyOrdered;
 		this.date=date;
+		prod=fProd.getProduct(opID);
+		prod.replenish(qtyOrdered);
+		
 	}
 	public String getOpID() {
 		return opID;
