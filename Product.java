0fab6c3bb247ec34ebade3cdd7c579df22a278ec
rmit-s3559846-julia-quach <s3559846@student.rmit.edu.au>
2016-05-11 13:58:18 +1000

    Add files via upload

--- Product.java
+++ Product.java
@@ -4,130 +4,65 @@ import supplier.FuncSupplier;
 
 public class Product {
 
-		private String pID, pName, sID, location;
-		private double unitPrice, stockLvl, replenishLvl, reorderQty, bulkQty, bulkDis, disPrice;
+	private String pID, pName, sID, location;
+	private double unitPrice, disPrice;
 
-		FuncSupplier fSupp = new FuncSupplier();
-		
-		
-		public Product (String pID, String pName, double unitPrice, double stockLvl, 
-						double replenishLvl, double reorderQty, String sID, String location, 
-						double bulkQty, double bulkDis, double disPrice)
-		{
-			this.pID = pID;
-			this.pName = pName;
-			this.unitPrice = unitPrice;
-			this.stockLvl = stockLvl;
-			this.replenishLvl = replenishLvl;
-			this.reorderQty = reorderQty;			
-			this.sID = sID;
-			this.location = location;
-			this.bulkQty = bulkQty;
-			this.bulkDis = bulkDis;
-			this.disPrice = disPrice;
-		}
-		
+	FuncSupplier fSupp = new FuncSupplier();
 
-		public String getpID() {
-			return pID;
-		}
+	public Product (String pID, String pName, double unitPrice, String sID, String location,
+			double disPrice)
+	{
+		this.pID = pID;
+		this.pName = pName;
+		this.unitPrice = unitPrice;
+		this.sID = sID;
+		this.location = location;
+		this.disPrice = disPrice;
+	}
+	public String getpID() {
+		return pID;
+	}
+	public void setpID(String pID) {
+		this.pID = pID;
+	}
+	public String getpName() {
+		return pName;
+	}
+	public void setpName(String pName) {
+		this.pName = pName;
+	}
+	public String getsID() {
+		return sID;
+	}
+	public void setsID(String sID) {
+		this.sID = sID;
+	}
+	public String getLocation() {
+		return location;
+	}
+	public void setLocation(String location) {
+		this.location = location;
+	}
+	public double getUnitPrice() {
+		return unitPrice;
+	}
+	public void setUnitPrice(double unitPrice) {
+		this.unitPrice = unitPrice;
+	}
+	public double getDisPrice() {
+		return disPrice;
+	}
+	public void setDisPrice(double disPrice) {
+		this.disPrice = disPrice;
+	}
+	public FuncSupplier getfSupp() {
+		return fSupp;
+	}
 
-
-		public String getpName() {
-			return pName;
-		}
-
-
-		public String getsID() {
-			return sID;
-		}
-
-
-		public String getLocation() {
-			return location;
-		}
-
-
-		public double getUnitPrice() {
-			return unitPrice;
-		}
-
-
-		public double getStockLvl() {
-			return stockLvl;
-		}
-
-
-		public double getReplenishLvl() {
-			return replenishLvl;
-		}
-
-
-		public double getReorderQty() {
-			return reorderQty;
-		}
-
-
-		public double getBulkQty() {
-			return bulkQty;
-		}
-
-
-		public double getBulkDis() {
-			return bulkDis;
-		}
-
-
-		public double getDisPrice() {
-			return disPrice;
-		}		
-		
-		/*
-		 * always successful
-		 * as customers have the items in their hands
-		 * 
-		 * only called when transaction finishes and payment is done
-		 */
-		public void sold(double qty){
-			stockLvl -= qty;
-		}
-
-		public void replenish(double qty) {
-			stockLvl += qty;
-			System.out.println("Replenish Successful!");
-			System.out.printf("New stock level of %s is %.2f\n\n", pName, stockLvl);			
-		}
-
-		public void addItemInfo(){
-			System.out.println();
-			System.out.println("-------------------------------------");
-			System.out.println("        Product Information"          );
-			System.out.println("-------------------------------------");
-			System.out.println(" I.D.:               " + pID          );
-			System.out.println(" Name:               " + pName        );
-			System.out.printf(" Price:              $%.2f\n", unitPrice);
-			System.out.println(" Stock Level:        " + stockLvl     );
-			System.out.println(" Bulk min amount:    " + bulkQty      );
-			System.out.printf(" Bulk discount:      %.0f", bulkDis*100);
-			System.out.println("% off");
-			
-			if(unitPrice > disPrice)	
-				System.out.printf(" Current Promotion:  $%.2f ONLY\n", disPrice);
-			System.out.println("-------------------------------------");
-			System.out.println();
-		}
-		
-		public void searchInfo(){
-			System.out.println();
-			System.out.println("-------------------------------------");
-			System.out.println("        Product Information"          );
-			System.out.println("-------------------------------------");
-			System.out.println(" I.D.:               " + pID          );
-			System.out.println(" Name:               " + pName        );
-			System.out.printf(" Price:              $%.2f\n", unitPrice );
-			System.out.println(" Stock Level:        " + stockLvl     );
-			System.out.println(" Location:           " + location     );
-			System.out.println("-------------------------------------");
-			System.out.println();
-		}	
+	/*
+	 * always successful
+	 * as customers have the items in their hands
+	 * 
+	 * only called when transaction finishes and payment is done
+	 */
 }
--- Products.java
+++ Products.java
@@ -9,6 +9,8 @@ import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.StringTokenizer;
 
+import product.NPProduct;
+import product.PProduct;
 import product.Product;;
 
 public class Products 
@@ -24,11 +26,23 @@ public class Products
 	 */
 	protected void productStartUp()
 	{		
-		products.add(new Product("p101", "banana", 2.00, 500.0, 100.0, 400.0, "s200", "1.2", 10.0, .10, 2.00));
-		products.add(new Product("p102", "grapes", 4.50, 200.0, 50.0, 150.0, "s201", "1.8", 8.0, .10, 3.50));
-		products.add(new Product("p103", "chocolate", 6.50, 150.0, 50.0, 100.0, "s201", "10.6", 20.0, .30, 5.00));
-		products.add(new Product("p104", "sugar", 3.00, 200.0, 50.0, 150.0, "s202", "9.2", 10.0, .15, 3.00));
-		products.add(new Product("p105", "flour", 2.50, 250.0, 70.0, 170.0, "s202", "9.5", 20.0, .10, 2.50));
+		products.add(new PProduct("pp101","banana",2.00,"s200","1.2",2.00,500.00,100.00,400.00,10.00,0.10));
+		products.add(new PProduct("pp102","grapes",4.50,"s201","1.8",3.50,200.00,50.00,150.00,8.00,0.10));
+		products.add(new NPProduct("np103","chocolate",6.50,"s201","10.6",5.00,150,50,100,20,0.30));
+		products.add(new NPProduct("np104","sugar",3.00,"s202","9.2",3.00,200,50,150,10,0.15));
+		products.add(new NPProduct("np105","flour",2.50,"s202","9.5",2.50,250,70,170,20,0.10));
+		
+		products.add(new PProduct("pp106","onion",13.60,"s200","1.3",13.00,500.00,100.00,400.00,10.00,0.10));
+		products.add(new PProduct("pp107","garlic",15.50,"s201","1.4",15.50,200.00,50.00,150.00,8.00,0.10));
+		products.add(new NPProduct("np108","soda",2.50,"s201","8.6",2.00,150,50,100,24,0.10));
+		products.add(new NPProduct("np109","gum",3.00,"s202","3.8",3.00,200,50,150,10,0.15));
+		products.add(new NPProduct("np110","chips",2.50,"s202","4.5",2.00,250,70,170,20,0.10));
+		
+		products.add(new PProduct("pp111","cucumber",5.00,"s200","1.2",5.00,500.00,100.00,400.00,10.00,0.10));
+		products.add(new PProduct("pp112","tomato",5.50,"s201","1.8",5.50,200.00,50.00,150.00,10.00,0.10));
+		products.add(new NPProduct("np113","tuna",1.50,"s201","6.6",1.20,150,50,100,30,0.05));
+		products.add(new NPProduct("np114","beans",3.00,"s202","5.2",3.00,200,50,150,20,0.15));
+		products.add(new NPProduct("np115","pasta",2.50,"s202","4.7",2.50,250,70,170,50,0.05));
 	}	
 	
 	/*
@@ -47,22 +61,46 @@ public class Products
 		 */
 		for (int i=0; i<products.size(); i++)
 		{
-			Product product = products.get(i);
+			if(products.get(i)instanceof PProduct)
+			{
+			PProduct product = (PProduct) products.get(i);
 			
 			String pID = product.getpID();
 			String pName = product.getpName();
 			double unitPrice = product.getUnitPrice();
+			String sID = product.getsID(); 
+			String location = product.getLocation();
+			double disPrice = product.getDisPrice();
 			double stockLvl = product.getStockLvl();
 			double replenishLvl = product.getReplenishLvl();
 			double reorderQty = product.getReorderQty();
-			String sID = product.getsID(); 
-			String location = product.getLocation();
 			double bulkQty = product.getBulkQty();
 			double bulkDis = product.getBulkDis(); 
-			double disPrice = product.getDisPrice();
+		
 			
-			pw.printf("%s|%s|%.2f|%.2f|%.2f|%.2f|%s|%s|%.2f|%.2f|%.2f\n", 
-					pID, pName, unitPrice, stockLvl, replenishLvl, reorderQty, sID, location, bulkQty, bulkDis, disPrice);			
+			pw.printf("%s|%s|%.2f|%s|%s|%.2f|%.2f|%.2f|%.2f|%.2f|%.2f\n", 
+					pID, pName, unitPrice, sID, location, disPrice, stockLvl, replenishLvl, reorderQty, bulkQty, bulkDis);
+			}
+			else if(products.get(i)instanceof NPProduct)
+			{
+			NPProduct product = (NPProduct) products.get(i);
+			
+			String pID = product.getpID();
+			String pName = product.getpName();
+			double unitPrice = product.getUnitPrice();
+			String sID = product.getsID(); 
+			String location = product.getLocation();
+			double disPrice = product.getDisPrice();
+			int stockLvl = (int) product.getStockLvl();
+			int replenishLvl = (int) product.getReplenishLvl();
+			int reorderQty = (int) product.getReorderQty();
+			int bulkQty = (int) product.getBulkQty();
+			int bulkDis = (int) product.getBulkDis(); 
+			
+			
+			pw.printf("%s|%s|%.2f|%s|%s|%d|%d|%d|%d|%d|%d\n", 
+					pID, pName, unitPrice, sID, location, disPrice, stockLvl, replenishLvl, reorderQty, bulkQty, bulkDis);
+			}
 		}
 		pw.close();
 	}
@@ -84,22 +122,38 @@ public class Products
 		while (sc.hasNext())
 		{
 			String line = sc.nextLine();
-			
 			StringTokenizer st = new StringTokenizer(line, "|");
 			
 			String pID = st.nextToken();
 			String pName = st.nextToken();
 			double unitPrice = Double.parseDouble(st.nextToken());
+			String sID = st.nextToken();
+			String location = st.nextToken();
+			double disPrice = Double.parseDouble(st.nextToken());
+			
+			if(pID.contains("pp"))
+			{
 			double stockLvl = Double.parseDouble(st.nextToken());
 			double replenishLvl = Double.parseDouble(st.nextToken());
 			double reorderQty = Double.parseDouble(st.nextToken());
-			String sID = st.nextToken();
-			String location = st.nextToken();
 			double bulkQty = Double.parseDouble(st.nextToken());
 			double bulkDis = Double.parseDouble(st.nextToken());
-			double disPrice = Double.parseDouble(st.nextToken());
 			
-			products.add(new Product(pID, pName, unitPrice, stockLvl, replenishLvl, reorderQty, sID, location, bulkQty, bulkDis, disPrice));
+			
+			products.add(new PProduct(pID, pName, unitPrice, sID, location, disPrice, stockLvl, replenishLvl, reorderQty, bulkQty, bulkDis));
+			}
+			else if(pID.contains("np"))
+			{
+			int stockLvl = Integer.parseInt(st.nextToken());
+			int replenishLvl = Integer.parseInt(st.nextToken());
+			int reorderQty = Integer.parseInt(st.nextToken());
+			int bulkQty = Integer.parseInt(st.nextToken());
+			double bulkDis = Double.parseDouble(st.nextToken());
+			
+			
+			products.add(new NPProduct(pID, pName, unitPrice, sID, location, disPrice, stockLvl, replenishLvl, reorderQty, bulkQty, bulkDis));
+			}
+			
 		}
 		sc.close();
 	}	
