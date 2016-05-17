6f156ffd7fbdf199b581dc9c9c239ad3757e5128
rmit-s3444615-ryan-teo <s3444615@student.rmit.edu.au>
2016-05-17 19:21:37 +1000

    Deleted all files to restart

--- Manager.java
+++
@@ -1,9 +0,0 @@
-package employee;
-
-public class Manager extends Employee {
-
-	public Manager(String eID, String eName, String password) {
-		super(eID, eName, password);
-	}
-
-}
--- SaleStaff.java
+++
@@ -1,9 +0,0 @@
-package employee;
-
-public class SaleStaff extends Employee {
-
-	public SaleStaff(String eID, String eName, String password) {
-		super(eID, eName, password);
-	}
-
-}
