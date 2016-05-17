9ce2fea1de0614d69322f56c7bbb4b2787ca66d0
rmit-s3444615-ryan-teo <s3444615@student.rmit.edu.au>
2016-05-17 19:07:38 +1000

    merge

--- src/report/Report.java
+++ src/report/Report.java
@@ -23,8 +23,8 @@ public class Report {
 			SaleLineItem transaction = trans.get(i); //DELETO
 			Date date = setDate(transaction.getDate());
 			if(checkDate(date, date1, date2)==true){
-				String ipName = transaction.getIpName();
-				Product prod = fProd.getProduct(ipName);
+				String ipName = transaction.getIpName();//helo
+				Product prod = fProd.getProduct(ipName);//myname jeff
 				Double qty = transaction.getQty();
 				Double revenue = transaction.getRevenue();
 				for(int j=0;j<SalesReport.size();j++){
