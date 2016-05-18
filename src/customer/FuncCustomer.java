62f2ee8879b6dc45fcaf6143b4ab1ebfc59f7966
rmit-s3510376-Harry-Utomo <s3510376@student.rmit.edu.au>
2016-05-18 16:50:36 +1000

    Update FuncCustomer.java

--- src/customer/FuncCustomer.java
+++ src/customer/FuncCustomer.java
@@ -5,7 +5,7 @@ import exceptions.NotFoundException;
 
 public class FuncCustomer{
 	private Customers custs = new Customers(); //HARRY's CHANGE
-	public Customer getCustomer(String cID) throws NotFoundException{
+	public Customer getCustomer(String cID) throws NotFoundException{//hello
 		Customer cust = null;
 		for(int i=0; i<custs.getCustomers().size(); i++){
 			if(custs.getCustomers().get(i).getcID().equals(cID))
