93e633350d840acf489eb6457d8adb12784795df
rmit-s3558319-lei-zheng <s3558319@student.rmit.edu.au>
2016-05-19 11:55:23 +1000

    Grace's changes: some implementations and menu updates
    I've implemented manager's functions of my part (add new product & edit
    current product)
    Also, menu for both sale staff and manager are built.

--- src/menu/SaleMenu.java
+++ src/menu/SaleMenu.java
@@ -3,13 +3,57 @@ package menu;
 import java.util.Scanner;
 
 import employee.Employee;
+import employee.SaleStaff;
 
 public class SaleMenu {
 
 	public void runSaleMenu(Employee emp, Scanner sc) 
 	{
-		System.out.printf("Function not implemented yet...\n\n");
-		
+		String input = null;
+		  do{
+				printMenu();
+				input = sc.nextLine();
+				processInput(input,sc,emp);
+				
+		  }while(!input.equals("Q"));		
 	}
 
+	private void printMenu()
+	{
+		System.out.println("--------Sale Menu--------");
+		System.out.println("1. Add New Customer");
+		System.out.println("2. Top Up");
+		System.out.println("3. Override Transaction");
+		System.out.println("Q. Quit");
+		System.out.println();
+		System.out.print("Please enter your choice: ");
+	}
+
+	private void processInput(String input, Scanner sc, Employee emp)
+	{
+		SaleStaff sStaff = (SaleStaff) emp;
+		
+		switch(input){
+		
+		// add new customer
+		case "1":
+			//TODO
+			break;
+			
+		// top up
+		case "2": 
+			//TODO
+			break;
+		 
+		// override transaction
+		case "3":
+			// TODO
+			break;
+			
+		// quit
+		case "Q":
+			System.out.print("Quitting admin mode...\n\n");
+			break;			
+		}
+	}
 }
--- src/product/FuncProduct.java
+++ src/product/FuncProduct.java
@@ -1,11 +1,9 @@
 package product;
 
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 import data.LoadData;
-import data.SaveData;
 import exceptions.*;
 import system.Helpers;
 
@@ -123,202 +121,6 @@ public class FuncProduct
 		Helpers.pause(sc);
 	 }
 	
-	public void addNewProduct(Scanner sc)
-	{
-		String pID, pName, sID, location, type;
-		int stockLvl, replenishLvl, reorderQty, bulkQty;
-		double unitPrice, pStockLvl,pReplenishLvl, pReorderQty, pBulkQty, bulkDis, disPrice;
-		
-		ArrayList<Product> products = load();
-				
-		// product ID
-		System.out.print("Please enter new Product I.D: ");
-		pID = sc.nextLine();
-		
-		// product name
-		System.out.print("Enter Product's name: ");
-		pName = sc.nextLine();
-		
-		// type of product
-		System.out.print("Enter type of product ('p' for perishable & 'n' for non-perishable): ");
-		type = sc.nextLine();
-		
-		// for perishable product
-		if (type.equals("p"))
-		{
-			// unit price
-			System.out.print("Enter price per kg: ");
-			unitPrice = Double.parseDouble(sc.nextLine());
-			
-			// stock level
-			System.out.print("Enter stock level (kg): ");
-			pStockLvl = Double.parseDouble(sc.nextLine());
-			
-			// replenish level
-			System.out.print("Enter minimum automatic replenish level (kg): ");
-			pReplenishLvl = Double.parseDouble(sc.nextLine());
-			
-			// reorder quantity
-			System.out.print("Enter automatic reorder quantity (kg): ");
-			pReorderQty = Double.parseDouble(sc.nextLine());
-			
-			// supplier
-			System.out.print("Enter Supplier I.D.: ");
-			sID = sc.nextLine();
-			
-			// location
-			System.out.print("Enter shelf location: ");
-			location = sc.nextLine();
-			
-			// bulk quantity
-			System.out.print("Enter minimum bulk quantity (kg) for discount: ");
-			pBulkQty = Double.parseDouble(sc.nextLine());
-			
-			// bulk discount
-			System.out.print("Enter bulk quantity discount (eg. 0.2 for 20%): ");
-			bulkDis = Double.parseDouble(sc.nextLine());
-			
-			// promotional discount price
-			System.out.print("Enter promotional sale price if applicable, else enter original price: ");
-			disPrice = Double.parseDouble(sc.nextLine());
-			
-			// add product to the array list
-			products.add(new PProduct(pID, pName, unitPrice, sID, location, disPrice,
-								pStockLvl, pReplenishLvl, pReorderQty, pBulkQty, bulkDis));
-			System.out.println("Product added successfully!");
-		}
-				
-		// for non-perishable product
-		else if (type.equals("n"))
-		{
-			// unit price
-			System.out.print("Enter price per item: ");
-			unitPrice = Double.parseDouble(sc.nextLine());
-			
-			// stock level
-			System.out.print("Enter stock level (item): ");
-			stockLvl = Integer.parseInt(sc.nextLine());
-			
-			// replenish level
-			System.out.print("Enter minimum automatic replenish level (item): ");
-			replenishLvl = Integer.parseInt(sc.nextLine());
-			
-			// reorder quantity
-			System.out.print("Enter automatic reorder quantity (item): ");
-			reorderQty = Integer.parseInt(sc.nextLine());
-			
-			// supplier
-			System.out.print("Enter Supplier I.D.: ");
-			sID = sc.nextLine();
-			
-			// location
-			System.out.print("Enter shelf location: ");
-			location = sc.nextLine();
-			
-			// bulk quantity
-			System.out.print("Enter minimum bulk quantity (item) for discount: ");
-			bulkQty = Integer.parseInt(sc.nextLine());
-			
-			// bulk discount
-			System.out.print("Enter bulk quantity discount (eg. 0.2 for 20%): ");
-			bulkDis = Double.parseDouble(sc.nextLine());
-			
-			// promotional discount price
-			System.out.print("Enter promotional sale price if applicable, else enter original price: ");
-			disPrice = Double.parseDouble(sc.nextLine());
-			
-			// add product to the array list
-			products.add(new PProduct(pID, pName, unitPrice, sID, location, disPrice,
-									stockLvl, replenishLvl, reorderQty, bulkQty, bulkDis));
-			System.out.println("Product added successfully!");
-		}		
-		
-		/*
-		 * save the array list to file
-		 */
-		save(products);
-	}
-	
-	public void editCurrentItem(Scanner sc)
-	{		
-		Product prod = null;
-		String pID;
-		int check = 0;
-		String choice;
-		
-		ArrayList<Product> products = load();
-		
-		/*
-		 * loop until product found
-		 */
-		do{
-			try {
-				System.out.print("Please enter product ID/Name: ");
-				pID = sc.nextLine();
-				
-				for (int i = 0; i < products.size(); i++)	
-					if (products.get(i).getpID().compareTo(pID) == 0 
-						|| products.get(i).getpName().compareTo(pID) == 0)
-					{
-						if (products.get(i)instanceof PProduct)
-						{
-							prod = (PProduct) products.get(i);
-						}
-						else if (products.get(i)instanceof NPProduct)
-						{
-							prod = (NPProduct) products.get(i);
-						}
-					}
-				
-				if (prod == null)
-				{
-					throw new NotFoundException(pID);
-				}
-				
-				check = 1;
-			} catch (NotFoundException e) {
-				e.printErrorMessage();
-			}
-		}while(check == 0);
-		
-		prod.addItemInfo();
-		
-		System.out.println("1. to edit bulk info");
-		System.out.println("2. to edit promotional sale price");
-		System.out.print("Please enter your choice: ");
-		choice = sc.nextLine();
-		
-		if(choice.equals("1"))
-		{
-			System.out.print("Enter the new minimum bulk quantity for discount: ");
-			if(prod instanceof PProduct)
-				((PProduct) prod).setBulkQty(Double.parseDouble(sc.nextLine()));
-			else if(prod instanceof NPProduct)
-				((NPProduct) prod).setBulkQty(Integer.parseInt(sc.nextLine()));
-			
-			System.out.print("Enter the new bulk quantity discount (eg. 0.2 for 20%): ");
-			if(prod instanceof PProduct)
-				((PProduct) prod).setBulkDis(Double.parseDouble(sc.nextLine()));
-			else if(prod instanceof NPProduct)
-				((NPProduct) prod).setBulkDis(Double.parseDouble(sc.nextLine()));			
-			
-			System.out.println("Bulk info editted successfully!");
-		}
-		
-		else if(choice.equals("2"))
-		{
-			System.out.print("Enter the new promotional sale price: ");
-			prod.setDisPrice(Double.parseDouble(sc.nextLine()));
-			
-			System.out.println("Promotional sale price editted successfully!");
-		}
-		
-		/*
-		 * save the array list to file
-		 */
-		save(products);
-	}
-	
 	private ArrayList<Product> load()
 	{
 		/*
@@ -335,14 +137,5 @@ public class FuncProduct
 		catch (Exception e)	{}
 		
 		return products;
-	}
-	
-	private void save(ArrayList<Product> products)
-	{
-		SaveData save = new SaveData();
-		
-		try {
-			save.saveProducts(products);
-		} catch (IOException e) {}
 	}
 }
