baf2f0946b69546cf5cfa2018e7bbe0d7c7c5839
rmit-s3558319-lei-zheng <s3558319@student.rmit.edu.au>
2016-05-23 14:49:29 +1000

    Grace: clean up data

--- src/data/AppendData.java
+++ src/data/AppendData.java
@@ -7,18 +7,15 @@ import java.io.PrintWriter;
 import java.util.ArrayList;
 
 import customer.Customer;
-import employee.Employee;
-import order.Order;
 import product.NPProduct;
 import product.PProduct;
 import product.Product;
 import sale.SaleLineItem;
-import supplier.Supplier;
 
 public class AppendData 
 {
 	/*
-	 * to save new customers 
+	 * to append new customers 
 	 * from array list to file
 	 */
 	public void appendCustomers(ArrayList<Customer> customers) throws IOException
@@ -46,62 +43,7 @@ public class AppendData
 	}
 	
 	/*
-	 * to save new employees
-	 * from array list to file
-	 */
-	public void appendEmployees(ArrayList<Employee> employees) throws IOException
-	{
-		/*
-		 * to create the file
-		 */
-		PrintWriter pw = new PrintWriter (new BufferedWriter (new FileWriter ("employees.txt", true)));
-		
-		/*
-		 * to write each element into the file
-		 */
-		for (int i=0; i<employees.size(); i++)
-		{
-			Employee employee = employees.get(i);
-			
-			String eID = employee.geteID();
-			String eName = employee.geteName();
-			String password = employee.getPassword();
-			
-			pw.printf("%s|%s|%s\n", eID, eName, password);			
-		}
-		pw.close();
-	}
-
-	/*
-	 * to save new orders
-	 * from array list to file
-	 */
-	public void appendOrders(ArrayList<Order> orders) throws IOException
-	{
-		/*
-		 * to create the file
-		 */
-		PrintWriter pw = new PrintWriter (new BufferedWriter (new FileWriter ("orders.txt", true)));
-		
-		/*
-		 * to write each element into the file
-		 */
-		for (int i=0; i<orders.size(); i++)
-		{
-			Order order = orders.get(i);
-			
-			String opID = order.getOpID();
-			String opName = order.getOpName();
-			double qtyOrdered = order.getQtyOrdered();
-			String date = order.getDate();
-			
-			pw.printf("%s|%s|%.2f|%s\n", opID, opName, qtyOrdered, date);			
-		}
-		pw.close();
-	}
-	
-	/*
-	 * to save new products 
+	 * to append new products 
 	 * from array list to file
 	 */
 	public void appendProducts(ArrayList<Product> products) throws IOException
@@ -161,35 +103,7 @@ public class AppendData
 	}
 	
 	/*
-	 * to save new suppliers
-	 * from array list to file
-	 */
-	public void appendSuppliers(ArrayList<Supplier> suppliers) throws IOException
-	{
-		/*
-		 * to create the file
-		 */
-		PrintWriter pw = new PrintWriter (new BufferedWriter (new FileWriter ("suppliers.txt", true)));
-		
-		/*
-		 * to write each element into the file
-		 */
-		for (int i=0; i<suppliers.size(); i++)
-		{
-			Supplier supplier = suppliers.get(i);
-			
-			String sID = supplier.getsID();
-			String sName = supplier.getsName();
-			String sPhone = supplier.getsPhone();
-			String sEmail = supplier.getsEmail();
-			
-			pw.printf("%s|%s|%s|%s\n", sID, sName, sPhone, sEmail);			
-		}
-		pw.close();
-	}
-	
-	/*
-	 * to save new transactions 
+	 * to append new transactions 
 	 * from array list to file
 	 */
 	public void appendTransactions(ArrayList<SaleLineItem> transactions) throws IOException
